95e08d9e607bf7dd710470e80cb09376665badac
gamela-francisco <205445001+gamela-francisco@users.noreply.github.com>
2026-07-01 17:37:33 +0100

    feat: implemenet full transaction CRUD endpoints with mock storage integration

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from typing import List
+from app.schemas import TransactionBase
+from app.storage import save_transaction, get_all_transactions
 
 # Initialise the FastAPI application instance
 app = FastAPI()
@@ -14,3 +17,28 @@ def read_root() -> dict:
     server is up and running correctly.
     """
     return {"message": "Welcome to FinTrack API"}
+
+@app.post("/transactions")
+def create_transaction(transaction: TransactionBase) -> dict:
+    """
+    Endpoint to log a new financial  transaction.
+
+    1. Receives data matching the TransactionBase schema.
+    2. The Pydantic 'bouncer' automatically validates it.
+    3. Converts the valid schema object to a standard Python dictionary.
+    4. Passes it to storage to be assigned an ID and saved.
+    """
+    # Convert Pydantic object to dictionary
+    transaction_dict = transaction.model_dump()
+
+    # Saving it using storage layer
+    saved_record = save_transaction(transaction_dict)
+
+    return saved_record
+
+@app.get("/transactions")
+def read_all_transactions() -> List[dict]:
+    """
+    Endpoint to retrieve all financial transactions.
+    """
+    return get_all_transactions()
